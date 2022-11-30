cbf407e42fbb65452558b99661799d3646bb7c9e
pwnd2e <104146035+pwnd2e@users.noreply.github.com>
2022-11-30 14:05:03 -0500

    palera1n-loader

--- palera1nLoader/ContentView.swift
+++ palera1nLoader/ContentView.swift
@@ -77,11 +77,11 @@ struct ContentView: View {
                 Image("palera1n-white")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .padding(.bottom, 0.0)
+                    .padding(.bottom, -3.0)
                     .frame(width: 68.0)
-                Text("")
+                Text("palera1n")
                     .font(.system(size: 40, weight: .bold))
-                    .padding()
+                    .padding(.bottom, -3.0)
                     
                     
                     
